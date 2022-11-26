4429f3f0054bf6e24393a6c889a20fa43ce52287
Javier Cerino Pulido <j.cerino@uniandes.edu.co>
2022-11-26 10:53:49 -0500

    Que no falle :3

--- measurements/measurements/views.py
+++ measurements/measurements/views.py
@@ -16,6 +16,14 @@ def check_variable(data):
             return True
     return False
 
+def check_place(data):
+    r = requests.get(settings.PATH_PLACE, headers={"Accept":"application/json"})
+    places = r.json()
+    for place in places:
+        if data["place"] == place["id"]:
+            return True
+    return False
+
 def MeasurementList(request):
     queryset = Measurement.objects.all()
     context = list(queryset.values('id', 'variable', 'value', 'unit', 'place', 'dateTime'))
@@ -34,7 +42,7 @@ def MeasurementCreate(request):
             measurement.save()
             return HttpResponse("successfully created measurement")
         else:
-            return HttpResponse("unsuccessfully created measurement. Variable does not exist")
+            return HttpResponse("unsuccessfully created measurement. Variable does not exist or Places does not exist")
 
 def MeasurementsCreate(request):
     if request.method == 'POST':
--- places/monitoring/settings.py
+++ places/monitoring/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'measurements',
+    'variables',
 ]
 
 MIDDLEWARE = [
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'measurements_db',
-        'USER': 'measurements_user',
+        'NAME': 'places_db',
+        'USER': 'places_user',
         'PASSWORD': 'isis2503',
-        'HOST': '10.206.0.3',
+        'HOST': '10.206.0.7',
         'PORT': '5432',
     }
 }
@@ -132,7 +132,3 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-
-PATH_VAR = "http://10.206.0.4:8080/variables/"
-
-PATH_PLACE = "http://10.206.0.8:8080/places/"
